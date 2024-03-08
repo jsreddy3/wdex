7dee89d09bf333eff804d537322d1ca656e58e54
Jaiden Reddy <jaidenreddy@gmail.com>
2024-03-07 20:22:22 -0800

    Initial Commit

---
+++ wdex/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wdex
+//
+//  Created by Jaiden Reddy on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wdex/wdexApp.swift
@@ -0,0 +1,17 @@
+//
+//  wdexApp.swift
+//  wdex
+//
+//  Created by Jaiden Reddy on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct wdexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
